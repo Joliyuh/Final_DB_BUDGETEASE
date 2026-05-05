db76ab0c83b3acd97fa1ac6e3b44b3da4670a450
Joliyuh <imyourjulie28@email.com>
2026-05-05 09:41:42 +0800

    login.php html

--- login.php
+++ login.php
@@ -32,3 +32,65 @@ if(isset($_POST['login'])){
 }
 ?>
 
+Login.php
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Login</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body class="login-page">
+
+<div class="login-card">
+
+    <h1 class="logo">
+        BudgetEase
+    </h1>
+
+    <h3 class="sub-title">
+        Login Account
+    </h3>
+
+    <form method="POST">
+
+        <input type="email"
+               name="email"
+               class="form-control"
+               placeholder="Email"
+               required>
+
+        <input type="password"
+               name="password"
+               class="form-control"
+               placeholder="Password"
+               required>
+
+        <button type="submit"
+                class="btn btn-success w-100"
+                name="login">
+
+            Login
+
+        </button>
+
+    </form>
+
+    <a href="register.php"
+       class="account-link d-block text-center mt-3">
+
+        Create Account
+
+    </a>
+
+</div>
+
+</body>
+
+</html>
