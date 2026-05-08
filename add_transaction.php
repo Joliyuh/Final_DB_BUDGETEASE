0a77b95624d7451e08b15b43f50ffe1882412aba
Joliyuh <imyourjulie28@gmail.com>
2026-05-08 10:15:32 +0800

    html

--- add_transaction.php
+++ add_transaction.php
@@ -42,3 +42,13 @@ if(isset($_POST['add'])){
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body class="main-body">
+<div class="main-container">        
+
+<div class="container mt-5">
+
+<div class="card p-4 login-card">
