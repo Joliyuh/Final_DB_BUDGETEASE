c38562e250eb5de67b92f2a41d90d97bf022b84b
Joliyuh <imyourjulie28@gmail.com>
2026-05-08 10:14:44 +0800

    links

--- add_transaction.php
+++ add_transaction.php
@@ -32,3 +32,13 @@ if(isset($_POST['add'])){
     header("Location: dashboard.php");
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Add Transaction</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
