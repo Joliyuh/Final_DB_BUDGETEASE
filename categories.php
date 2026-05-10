6c3a98c8911d1db372ed298ece7efbb46cd84dbc
Joliyuh <imyourjulie28@gmail.com>
2026-05-10 10:31:26 +0800

    links

--- categories.php
+++ categories.php
@@ -43,3 +43,13 @@ ORDER BY Category_Name ASC"
 
 );
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Categories</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="style.css">
